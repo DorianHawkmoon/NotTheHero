c6c28e2ce7b8cf1f1d4b7c755ca77cfdefeeff43
Dorian Hawkmoon <dorian.cadenas@gmail.com>
2018-02-04 18:09:39 +0100

    Basic inventory and drag and drop

---
+++ Assets/Scripts/Model/Item.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Item : MonoBehaviour { //monobehaviour temporal to use in inspector
+    public Sprite spriteItem;
+
+    public Sprite GetSprite() {
+        return spriteItem;
+    }
+}
+
---
+++ Assets/Scripts/UI/DragHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+
+public class DragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
+    //only one item at time
+    public static GameObject itemBeingDragged;
+
+
+    private Vector3 startPosition;
+
+    private Action<Vector3> onEndDragCallback;
+
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        itemBeingDragged = gameObject;
+        startPosition = transform.position;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        itemBeingDragged = null;
+        transform.position = startPosition;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        position.z = 0;
+        if (onEndDragCallback != null) {
+            onEndDragCallback(position);
+        }
+    }
+
+
+    public void RegisterOnEndDrag(Action<Vector3> callback) {
+        onEndDragCallback += callback;
+    }
+
+    public void UnregisterOnEndDrag(Action<Vector3> callback) {
+        onEndDragCallback -= callback;
+    }
+}
