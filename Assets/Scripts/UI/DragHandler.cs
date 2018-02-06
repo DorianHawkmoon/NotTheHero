6d19934e723353b644f28e344fa565f8a00bb1f9
Dorian Hawkmoon <dorian.cadenas@gmail.com>
2018-02-06 02:28:39 +0100

    Little improve to drag items

--- Assets/Scripts/UI/DragHandler.cs
+++ Assets/Scripts/UI/DragHandler.cs
@@ -2,32 +2,54 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using System;
+using UnityEngine.UI;
 
 public class DragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
     //only one item at time
     public static GameObject itemBeingDragged;
+    private Image image;
 
 
     private Vector3 startPosition;
 
     private Action<Vector3> onEndDragCallback;
+    private Action onStartDragCallback;
 
+    public void Start() {
+        image = GetComponent<Image>();
+    }
+
+    private GameObject DummieItem() {
+        GameObject dummie = new GameObject();
+        dummie.name = "draging object";
+        SpriteRenderer spriteRenderer = dummie.AddComponent<SpriteRenderer>();
+        spriteRenderer.sprite = image.sprite;
+        GameControllerTemporal.AddTemporal(dummie);
+        return dummie;
+    }
 
     public void OnBeginDrag(PointerEventData eventData) {
-        itemBeingDragged = gameObject;
-        startPosition = transform.position;
+        itemBeingDragged = DummieItem();
         GetComponent<CanvasGroup>().blocksRaycasts = false;
+        if (onStartDragCallback != null) {
+            onStartDragCallback();
+        }
     }
 
     public void OnDrag(PointerEventData eventData) {
-        transform.position = Input.mousePosition;
+        Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        position.z = 0;
+        itemBeingDragged.transform.position = position;
+
+        //TODO check collisions and tint it in red
     }
 
     public void OnEndDrag(PointerEventData eventData) {
+        Destroy(itemBeingDragged); //TODO change to a pool of objects
         itemBeingDragged = null;
-        transform.position = startPosition;
         GetComponent<CanvasGroup>().blocksRaycasts = true;
 
+        //TODO check if it's a good place
         Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         position.z = 0;
         if (onEndDragCallback != null) {
@@ -42,5 +64,13 @@ public class DragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndD
 
     public void UnregisterOnEndDrag(Action<Vector3> callback) {
         onEndDragCallback -= callback;
+    }
+
+    public void RegisterOnStartDrag(Action callback) {
+        onStartDragCallback += callback;
+    }
+
+    public void UnregisterOnStartDrag(Action callback) {
+        onStartDragCallback -= callback;
     }
 }
--- Assets/Scripts/UI/SlotInventory.cs
+++ Assets/Scripts/UI/SlotInventory.cs
@@ -4,31 +4,25 @@ using UnityEngine.UI;
 public class SlotInventory : MonoBehaviour {
     //data about the item
     public Item item;
-    //Gameobjet background containing the image and other details like the count
-    public GameObject background;
     //Gameobject dragable
     public GameObject dragable;
 
     //number of items left
     private int numberItems = 5;
     
-    
-
-
 	// Use this for initialization
 	void Start () {
         Sprite itemSprite = item.GetSprite();
-        background.GetComponent<Image>().sprite = itemSprite;
         dragable.GetComponent<Image>().sprite = itemSprite;
         dragable.GetComponent<DragHandler>().RegisterOnEndDrag(OnEndDragSlot);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        dragable.GetComponent<DragHandler>().RegisterOnStartDrag(OnStartDragSlot);
+    }
 
-    public void OnEndDragSlot(Vector3 position) {
+    private void OnStartDragSlot() {
+        
+    }
+
+    private void OnEndDragSlot(Vector3 position) {
         //put the item and substract the counter
         if (numberItems > 0) {
             Debug.Log("OnEndDragSlot.");
